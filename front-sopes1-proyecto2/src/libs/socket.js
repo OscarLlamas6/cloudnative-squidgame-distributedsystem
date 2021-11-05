54cbfff49d3ac83c6c4b02baefc6bd1d65eb932a
201709309 <josesantizo149@gmail.com>
2021-11-05 00:30:21 -0600

    [UPDATE] se corrigieron detalles en backend node y se agregaron variables de entorno en backend node y front react

--- front-sopes1-proyecto2/src/libs/socket.js
+++ front-sopes1-proyecto2/src/libs/socket.js
@@ -1,7 +1,15 @@
+import env from "react-dotenv"
 import io from 'socket.io-client'
+require('dotenv').config()
 
-let socket = io('http://localhost:8080')
 
-//let socket = io('https://api-sopes1-py1.ue.r.appspot.com/')
+
+//let socket = io('http://localhost:8080')
+
+let ruta = `http://${env.BACKEND_HOST}:${env.NODE_API_PORT}`
+
+let socket = io(ruta)
+
+console.log(ruta)
 
 export default socket;
