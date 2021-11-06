d8b479b4731c099430cffd7114fe254aa4464560
Oscar Llamas <oscarllamasusac@gmail.com>
2021-11-06 01:09:22 -0600

    [UPDATED] Frontend y Backend listos para deployar en Kubernetes :D

--- front-sopes1-proyecto2/src/libs/socket.js
+++ front-sopes1-proyecto2/src/libs/socket.js
@@ -4,12 +4,9 @@ require('dotenv').config()
 
 
 
-//let socket = io('http://localhost:8080')
+let socket = io({ path: "/socket.io" });
+//let socket = io('http://localhost:4000')
 
-let ruta = `http://${env.BACKEND_HOST}:${env.NODE_API_PORT}`
 
-let socket = io(ruta)
-
-console.log(ruta)
 
 export default socket;
